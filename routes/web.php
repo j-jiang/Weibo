4efaf3f05885fd40c809ea5f1dd961cdba3d78b9
jjha <1056692357@qq.com>
2019-01-30 10:45:27 +0800

    add test route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::get('/','StaticPagesController@home');
 Route::get('about','StaticPagesController@about');
 Route::get('help','StaticPagesController@help');
 
+Route::get('test',function (){
+    echo getenv('APP_KEY');
+});
+
+
+
 
 
 
